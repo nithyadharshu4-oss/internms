6ac0b1102704353d8ed3994c757f6b03301d6896
nithyadharshu4-oss <nithyadhrashu4@gmail.com>
2026-07-23 12:54:26 +0530

    fix: Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/internms/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
