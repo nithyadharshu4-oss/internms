8667b2bb92cbd6a5adc9c64900f568a49d99e8ae
nithyadharshu4-oss <nithyadhrashu4@gmail.com>
2026-08-13 15:08:31 +0530

    feat: add HrMyprofile component with styling and edit functionality

--- src/Components-Hr/HrMyprofile.jsx
+++ src/Components-Hr/HrMyprofile.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
 import "./HrMyprofile.css";
 
+import editIcon from "../assets/hr-modules/editIcon.png";
+import profileimage from"../assets/hr-modules/profileimage.jpg";
 
 export const HrMyprofile=()=> {
   return (
-    <div className="hr-myprofile">
-      <div className="hr-myprofile-content">
-      <h1>My Profile</h1>
-    </div>
-    </div>
+ <div className="hr-myprofile-main">
+ 
+ <div className="hr-myprofile-title">
+  <div>
+<h1>My Profile</h1>
+<p>View and update your personal information</p>
+</div>
+<div className="hr-myprofile-editbtn">
+<button>
+  <img src={editIcon} alt="edit"/>Edit Profile
+</button>
+</div>
+</div>
+
+<div className="hr-myprofile-card">
+<div className="hr-myprofileimage">
+  <img src={profileimage} alt="profile"/>
+
+</div>
+
+<div className="hr-myprofile-details">
+  <h3>Aiswariya</h3>
+  <h5>HR Manager</h5>
+  <h6>Employee ID: 12345</h6>
+  <p>Dedicated HR professional with experience in<br/>
+talent acquisition,employee relations,and<br/>
+performance management.</p>
+
+</div>
+
+<div className="hr-myprofile-bio">
+
+</div>
+
+</div>
+
+ 
+
+
+
+
+
+
+ </div>
   );
 }
